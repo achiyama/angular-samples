c63a80ae47dd40638ea848a491d15c398a94b061
Kohei Akiyama <kohei.akiyama@outlook.jp>
2023-07-24 23:27:45 +0900

    add standalone project

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Totorials
+
 - [Tour of Heroes](https://angular.jp/tutorial)
 - [Angular After Tutorial](https://zenn.dev/lacolaco/books/angular-after-tutorial)
+
+# Commands
+
+ルーティング付きモジュール作成
+
+```
+ng generate module モジュール名 --routing
+```
---
+++ src/app/standalone/standalone.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StandaloneRoutingModule } from './standalone-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, StandaloneRoutingModule],
+})
+export class StandaloneModule {}
